208ce4139a4d671c21c4516f7bf4b08956cbc9dd
Jay.R <jwatson2022@protonmail.com>
2022-09-13 07:29:55 +0100

    doc: adding comments on best practice for interface

--- classes/src/com/jr/vehicles/Registration.java
+++ classes/src/com/jr/vehicles/Registration.java
@@ -1,5 +1,7 @@
 package src.com.jr.vehicles;
 
+// most optimal interface should only have one method.
+// implement interfaces that shouldn't be split into abstract classes
 public interface Registration {
     public boolean isRegisteredForRoad();
     public boolean isRegisteredForFlying();
