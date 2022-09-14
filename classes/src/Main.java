e2c6f032b370c2607be34e86ec554cc2da7cea06
Jay.R <jwatson2022@protonmail.com>
2022-09-14 07:54:50 +0100

    doc: order of execution comments

--- classes/src/Main.java
+++ classes/src/Main.java
@@ -4,6 +4,13 @@ import src.com.jr.vehicles.Vehicle;
 
 public class Main {
 
+    // Flow of control a.k.a order in which our code is executed.
+    // in Java the starting point of execution is from the below main method. 
+    // The main method will be in the Java Call Stack. 
+    // Java uses call stack to keep track of the order of execution of method. 
+    // Each method or function is added to call stack as Stack Frames. They are added in a Last-In-First-Out basis. 
+    // The stack frames are popped once a return call is made.
+
     public static void main(String[] args){
 
         // Here Car.class extends from Vehicle.class and also inherits from Object.class
